a4f805155abe8c6247582df4bcdb664e5414b098
edison <hydra1983@gmail.com>
2013-04-25 15:29:35 +0800

    add sorted_for

--- _plugins/sorted_for.rb
+++ _plugins/sorted_for.rb
@@ -1,8 +1,12 @@
 module Jekyll
   class SortedForTag < Liquid::For
     def render(context)
-      sorted_collection = context[@collection_name].dup
-      sorted_collection.sort_by! { |i| i.to_liquid[@attributes['sort_by']] }
+      sorted_collection = []
+      sort_by = @attributes['sort_by']
+      context[@collection_name].dup.each { |i|
+        sorted_collection.push(i) unless i.to_liquid[sort_by].nil?
+      }
+      sorted_collection.sort_by! { |i| i.to_liquid[sort_by] }
 
       sorted_collection_name = "#{@collection_name}_sorted".sub('.', '_')
       context[sorted_collection_name] = sorted_collection
